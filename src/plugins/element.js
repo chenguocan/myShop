b33e4e3c586e6398085e8a2858e25f5cc611cf23
chenguocan <2622870670@qq.com>
2020-12-13 10:57:49 +0800

    登录样式

---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import { Button,Input,FormItem,Form } from 'element-ui';
+
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(FormItem)
+Vue.use(Form)
---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loginPage">
+    <div class="login">
+      <h1> 登录系统</h1>
+      <div class="login-form">
+        <el-form ref="form" :model="loginForm">
+          <el-form-item >
+            <el-input v-model="loginForm.username" placeholder="请输入用户"></el-input>
+          </el-form-item>
+          <el-form-item >
+            <el-input v-model="loginForm.password" placeholder="请输入密码" ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" class="login-btn">登录</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      loginForm:{
+        username:'',
+        password:'',
+      }
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.loginPage{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eaeaea;
+  .login{
+    width: 500px;
+    height: 300px;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .login-form{
+      width: 300px;
+      margin: 20px 40px;
+      .login-btn{
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
